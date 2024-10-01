b535d063dbc1356d59c9afdcde3d3d1a1314ba70
Jonas Meeuws <jonas.meeuws@gmail.com>
2024-10-01 19:47:11 +0200

    rewrite: Deletions

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-scp -r main root@10.0.0.10:ledstrip_sandbox/
-ssh root@10.0.0.10 "cd ledstrip_sandbox/ && cmake --build build"
--- deploy_editor.sh
+++
@@ -1,7 +0,0 @@
-set -x
-cd editor
-npm ci
-npm run build
-zip editor.zip dist/*
-scp editor.zip root@10.0.0.10:/tmp/editor.zip
-ssh root@10.0.0.10 "set -x && cd /tmp && unzip -u editor.zip && cd dist && rm -r /var/www/html/editor/* && cp -r * /var/www/html/editor && chown -R www-data:www-data /var/www/html/editor"
