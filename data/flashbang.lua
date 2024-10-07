2d6a3ec25c73331a39d31b8319a0a8462d45f568
Jonas Meeuws <jonas.meeuws@gmail.com>
2024-10-07 22:01:59 +0200

    Make flashbang halfbright

--- data/flashbang.lua
+++ data/flashbang.lua
@@ -2,7 +2,7 @@ while true do
     while not state.activeContainsThisScript() do
         time.waitFrames(1)
     end
-    led.setAll(0xFF, 0xFF, 0xFF)
+    led.setAll(0x80, 0x80, 0x80)
     time.waitFrames(2)
     led.setAll(0, 0, 0)
     state.setDefaultActive()
