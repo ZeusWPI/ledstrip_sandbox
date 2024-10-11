5e85c5aa7e3f5f796de5a372c8a4fc53b3ff581b
Jonas Meeuws <jonas.meeuws@gmail.com>
2024-10-11 12:20:37 +0200

    Serve index.html for all frontend pages

--- source/webserver/webserver.d
+++ source/webserver/webserver.d
@@ -9,10 +9,10 @@ import webserver.rest_api;
 import webserver.rest_api_impl;
 
 import vibe.data.json;
-import vibe.http.fileserver : HTTPFileServerSettings, serveStaticFiles;
+import vibe.http.fileserver : HTTPFileServerSettings, serveStaticFiles, serveStaticFile;
 import vibe.http.router : URLRouter;
 import vibe.http.server : HTTPListener, HTTPServerOption, HTTPServerRequest, HTTPServerRequestDelegateS,
-    HTTPServerResponse, HTTPServerSettings, listenHTTP;
+    HTTPServerResponse, HTTPServerSettings, listenHTTP, staticRedirect;
 import vibe.web.rest : path, registerRestInterface, RestInterfaceSettings;
 
 @safe:
@@ -27,6 +27,7 @@ class Webserver
     private RestApi m_restApi;
     private HTTPFileServerSettings m_fileServerSettings;
     private HTTPServerRequestDelegateS m_fileServer;
+    private HTTPServerRequestDelegateS m_indexFileServer;
     private URLRouter m_router;
     private HTTPListener m_listener;
 
@@ -44,9 +45,14 @@ class Webserver
 
         m_fileServerSettings = new HTTPFileServerSettings();
         m_fileServer = serveStaticFiles("public", m_fileServerSettings);
+        m_indexFileServer = serveStaticFile("public/index.html", m_fileServerSettings);
 
         m_router = new URLRouter;
         m_router.registerRestInterface(m_restApi, m_restApiSettings);
+        m_router.get("/", m_indexFileServer);
+        m_router.get("/statesSegments", m_indexFileServer);
+        m_router.get("/scripts", m_indexFileServer);
+        m_router.get("/config", m_indexFileServer);
         m_router.get("*", m_fileServer);
     }
 
