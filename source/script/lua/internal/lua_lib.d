98f96a611938a57ef5741ff78fd7b7ea5851ee07
Jonas Meeuws <jonas.meeuws@gmail.com>
2024-10-12 16:45:25 +0200

    Lua docs generation

--- source/script/lua/internal/lua_lib.d
+++ source/script/lua/internal/lua_lib.d
@@ -1,4 +1,5 @@
 module script.lua.internal.lua_lib;
+// dfmt off
 
 import ledstrip.led : Led;
 import ledstrip.ledstrip : Ledstrip;
@@ -17,19 +18,22 @@ import std.datetime : Clock;
 import std.exception : basicExceptionCtors, enforce;
 import std.format : f = format;
 import std.string : toStringz;
-import std.traits : EnumMembers;
+import std.traits : EnumMembers, isFunctionPointer;
 
 import vibe.core.core : sleep;
 import vibe.core.log;
 
 import bindbc.lua.v51 : lua_getglobal;
 
-import lumars : LuaFunc, LuaNil, LuaTable, LuaValue, LuaVariadic;
+import lumars : LuaFunc, LuaNil, LuaTable, LuaValue, LuaVariadic, EmmyLuaBuilder, addFunction;
+import lumars;
 
 @safe:
 
 class LuaLib
 {
+    private alias enf = enforce!LuaLibException;
+
     @disable this();
     @disable this(ref typeof(this));
 
@@ -50,72 +54,168 @@ static:
             return scriptTask.luaState.get!T(scriptTask.luaState.top);
         }
 
-        void addGlobalToTable(T = LuaFunc)(LuaTable t, string k) => t[k] = getGlobal!T(k);
-
         try
         {
             LuaTable env = createTable;
+            EmmyLuaBuilder elb;
+
+            void ensureModuleExists(string module_)
+            {
+                bool moduleExists;
+                env.tryGet!LuaTable(module_, moduleExists);
+                if (!moduleExists)
+                    env.set(module_, createTable);
+            }
+
+            void register(T, DocT = T)(T val, string module_, string name, string description)
+            {
+                if (module_.length)
+                {
+                    ensureModuleExists(module_);
+                    env.get!LuaTable(module_).set(name, val);
+                }
+                else
+                {
+                    env.set(name, val);
+                }
+
+                static if (isFunctionPointer!DocT)
+                {
+                    elb.addFunction!DocT(module_, name, description);
+                }
+                else
+                {
+                    elb.addValue!DocT(module_, name, description);
+                }
+            }
+
+            void registerGlobal(T, DocT = T)(string module_, string name, string description)
+            {
+                T val;
+                if (module_.length)
+                    val = getGlobal!LuaTable(module_).get!T(name);
+                else
+                    val = getGlobal!T(name);
+                register!(T, DocT)(val, module_, name, description);
+            }
 
             // Values
-            addGlobalToTable!LuaValue(env, "_VERSION");
+            registerGlobal!(string)("", "_VERSION", "");
 
             // Core
-            addGlobalToTable(env, "type");
+            registerGlobal!(LuaFunc)("", "type", "");
 
             // Error handling
-            addGlobalToTable(env, "assert");
-            addGlobalToTable(env, "error");
-            addGlobalToTable(env, "pcall");
-            addGlobalToTable(env, "xpcall");
+            registerGlobal!(LuaFunc)("", "assert", "");
+            registerGlobal!(LuaFunc)("", "error",  "");
+            registerGlobal!(LuaFunc)("", "pcall",  "");
+            registerGlobal!(LuaFunc)("", "xpcall", "");
 
             // String ops
-            addGlobalToTable(env, "tonumber");
-            addGlobalToTable(env, "tostring");
-            addGlobalToTable(env, "unpack");
-            addGlobalToTable!LuaTable(env, "string"); // Module
+            registerGlobal!(LuaFunc)("",       "tonumber", "");
+            registerGlobal!(LuaFunc)("",       "tostring", "");
+            registerGlobal!(LuaFunc)("",       "unpack",   "");
+            registerGlobal!(LuaFunc)("string", "byte",     "");
+            registerGlobal!(LuaFunc)("string", "char",     "");
+            registerGlobal!(LuaFunc)("string", "dump",     "");
+            registerGlobal!(LuaFunc)("string", "find",     "");
+            registerGlobal!(LuaFunc)("string", "format",   "");
+            registerGlobal!(LuaFunc)("string", "gmatch",   "");
+            registerGlobal!(LuaFunc)("string", "gsub",     "");
+            registerGlobal!(LuaFunc)("string", "len",      "");
+            registerGlobal!(LuaFunc)("string", "lower",    "");
+            registerGlobal!(LuaFunc)("string", "match",    "");
+            registerGlobal!(LuaFunc)("string", "rep",      "");
+            registerGlobal!(LuaFunc)("string", "reverse",  "");
+            registerGlobal!(LuaFunc)("string", "sub",      "");
+            registerGlobal!(LuaFunc)("string", "upper",    "");
 
             // Table ops
-            addGlobalToTable(env, "next");
-            addGlobalToTable(env, "pairs");
-            addGlobalToTable(env, "ipairs");
-            addGlobalToTable(env, "select");
-            addGlobalToTable!LuaTable(env, "table");
+            registerGlobal!(LuaFunc)("",      "ipairs", "");
+            registerGlobal!(LuaFunc)("",      "next",   "");
+            registerGlobal!(LuaFunc)("",      "pairs",  "");
+            registerGlobal!(LuaFunc)("",      "select", "");
+            registerGlobal!(LuaFunc)("table", "concat", "");
+            registerGlobal!(LuaFunc)("table", "insert", "");
+            registerGlobal!(LuaFunc)("table", "maxn",   "");
+            registerGlobal!(LuaFunc)("table", "remove", "");
+            registerGlobal!(LuaFunc)("table", "sort",   "");
 
-            // Other modules
-            addGlobalToTable!LuaTable(env, "bit");
-            addGlobalToTable!LuaTable(env, "math");
+            // Math
+            registerGlobal!(LuaFunc  )("math", "abs",        "");
+            registerGlobal!(LuaFunc  )("math", "acos",       "");
+            registerGlobal!(LuaFunc  )("math", "asin",       "");
+            registerGlobal!(LuaFunc  )("math", "atan",       "");
+            registerGlobal!(LuaFunc  )("math", "atan2",      "");
+            registerGlobal!(LuaFunc  )("math", "ceil",       "");
+            registerGlobal!(LuaFunc  )("math", "cos",        "");
+            registerGlobal!(LuaFunc  )("math", "cosh",       "");
+            registerGlobal!(LuaFunc  )("math", "deg",        "");
+            registerGlobal!(LuaFunc  )("math", "exp",        "");
+            registerGlobal!(LuaFunc  )("math", "floor",      "");
+            registerGlobal!(LuaFunc  )("math", "fmod",       "");
+            registerGlobal!(LuaFunc  )("math", "frexp",      "");
+            registerGlobal!(LuaNumber)("math", "huge",       ""); // Value
+            registerGlobal!(LuaFunc  )("math", "ldexp",      "");
+            registerGlobal!(LuaFunc  )("math", "log",        "");
+            registerGlobal!(LuaFunc  )("math", "log10",      "");
+            registerGlobal!(LuaFunc  )("math", "max",        "");
+            registerGlobal!(LuaFunc  )("math", "min",        "");
+            registerGlobal!(LuaFunc  )("math", "modf",       "");
+            registerGlobal!(LuaNumber)("math", "pi",         "");
+            registerGlobal!(LuaFunc  )("math", "pow",        "");
+            registerGlobal!(LuaFunc  )("math", "rad",        "");
+            registerGlobal!(LuaFunc  )("math", "random",     "");
+            registerGlobal!(LuaFunc  )("math", "randomseed", "");
+            registerGlobal!(LuaFunc  )("math", "sin",        "");
+            registerGlobal!(LuaFunc  )("math", "sinh",       "");
+            registerGlobal!(LuaFunc  )("math", "sqrt",       "");
+            registerGlobal!(LuaFunc  )("math", "tan",        "");
+            registerGlobal!(LuaFunc  )("math", "tanh",       "");
 
-            // Custom
-            env["log"] = &log;
-            env["led"] = () {
-                LuaTable led = createTable;
-                led["count"] = LedModule.count; // Value
-                led["set"] = &LedModule.set;
-                led["setSlice"] = &LedModule.setSlice;
-                led["setAll"] = &LedModule.setAll;
-                return led;
-            }();
-            env["state"] = () {
-                LuaTable state = createTable;
-                state["activeName"] = &StateModule.activeName;
-                state["activeContainsThisScript"] = &StateModule.activeContainsThisScript;
-                state["setActiveByName"] = &StateModule.setActiveByName;
-                state["setDefaultActive"] = &StateModule.setDefaultActive;
-                return state;
-            }();
-            env["time"] = () {
-                LuaTable time = createTable;
-                time["stdTimeHnsecs"] = &TimeModule.stdTimeHnsecs;
-                time["unixTimeSeconds"] = &TimeModule.unixTimeSeconds;
-                time["sleepMsecs"] = &TimeModule.sleepMsecs;
-                time["waitFrames"] = &TimeModule.waitFrames;
-                return time;
-            }();
-            env["mailbox"] = () {
-                LuaTable mailbox = createTable;
-                mailbox["consume"] = &MailboxModule.consume;
-                return mailbox;
-            }();
+            // Bit manipulation
+            registerGlobal!(LuaFunc)("bit", "arshift", "");
+            registerGlobal!(LuaFunc)("bit", "band",    "");
+            registerGlobal!(LuaFunc)("bit", "bnot",    "");
+            registerGlobal!(LuaFunc)("bit", "bor",     "");
+            registerGlobal!(LuaFunc)("bit", "bswap",   "");
+            registerGlobal!(LuaFunc)("bit", "bxor",    "");
+            registerGlobal!(LuaFunc)("bit", "lshift",  "");
+            registerGlobal!(LuaFunc)("bit", "rol",     "");
+            registerGlobal!(LuaFunc)("bit", "ror",     "");
+            registerGlobal!(LuaFunc)("bit", "rshift",  "");
+            registerGlobal!(LuaFunc)("bit", "tobit",   "");
+            registerGlobal!(LuaFunc)("bit", "tohex",   "");
+
+            // Custom globals
+            register(&log, "", "log", "");
+
+            // Led module
+            register(LedModule.count,     "led", "count",    ""); // Value
+            register(&LedModule.set,      "led", "set",      "");
+            register(&LedModule.setSlice, "led", "setSlice", "");
+            register(&LedModule.setAll,   "led", "setAll",   "");
+
+            // State module
+            register(&StateModule.activeName,               "state", "activeName", "");
+            register(&StateModule.activeContainsThisScript, "state", "activeContainsThisScript", "");
+            register(&StateModule.setActiveByName,          "state", "setActiveByName", "");
+            register(&StateModule.setDefaultActive,         "state", "setDefaultActive", "");
+            
+            // Time module
+            register(&TimeModule.stdTimeHnsecs,   "time", "stdTimeHnsecs",   "");
+            register(&TimeModule.unixTimeSeconds, "time", "unixTimeSeconds", "");
+            register(&TimeModule.sleepMsecs,      "time", "sleepMsecs",      "");
+            register(&TimeModule.waitFrames,      "time", "waitFrames",      "");
+
+            // Mailbox module
+            register(&MailboxModule.consume, "mailbox", "consume", "");
+            
+            {
+                import std.file : write;
+
+                write("api.lua", elb.toString());
+            }
 
             return env;
         }
@@ -181,7 +281,7 @@ static:
 
         void set(uint index, ubyte r, ubyte g, ubyte b)
         {
-            enforce!LuaLibException(
+            enf(
                 index < constLeds.length,
                 f!`Lua script "%s" led.set: Led index %u out of bounds for segment with length %u`(
                     constScript.name, index, constLeds.length,
@@ -193,13 +293,13 @@ static:
 
         void setSlice(uint begin, uint end, ubyte r, ubyte g, ubyte b)
         {
-            enforce!LuaLibException(
+            enf(
                 begin <= end,
                 f!`Lua script "%s" led.setSlice: Begin index %u larger than end index %u`(
                     constScript.name, begin, end,
             ),
             );
-            enforce!LuaLibException(
+            enf(
                 end <= constLeds.length,
                 f!`Lua script "%s" led.setSlice: End index %u out of bounds for segment with length %u"`(
                     constScript.name, end, constLeds.length,
@@ -228,7 +328,8 @@ static:
         bool activeContainsThisScript()
         {
             const Script thisScript = LuaScriptTask.constInstance.script;
-            foreach (begin, const LedstripSegment seg; LedstripStates.constInstance.activeState.segments)
+            foreach (begin, const LedstripSegment seg; LedstripStates
+                .constInstance.activeState.segments)
                 if (seg.scriptName == thisScript.name)
                     return true;
             return false;
@@ -260,7 +361,7 @@ static:
         void sleepMsecs(long msecs)
         {
             string name = LuaScriptTask.constInstance.script.name;
-            enforce!LuaLibException(
+            enf(
                 msecs >= 0,
                 f!`Lua script "%s": Cannot sleep for less than %d msecs`(name, msecs),
             );
