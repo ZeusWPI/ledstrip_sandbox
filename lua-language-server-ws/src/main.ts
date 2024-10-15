33fbe7655c19a9f1027f5570706a384d17f1f28b
Jonas Meeuws <jonas.meeuws@gmail.com>
2024-10-15 12:43:55 +0200

    Lua language server over ws

---
+++ lua-language-server-ws/src/main.ts
@@ -0,0 +1,45 @@
+import { resolve } from 'path';
+import { IWebSocket, WebSocketMessageReader, WebSocketMessageWriter } from 'vscode-ws-jsonrpc';
+import { createConnection, createServerProcess, forward } from 'vscode-ws-jsonrpc/server';
+import { WebSocketServer } from 'ws';
+
+export const launch = (socket: IWebSocket) => {
+    const reader = new WebSocketMessageReader(socket);
+    const writer = new WebSocketMessageWriter(socket);
+
+    const socketConnection = createConnection(
+        reader,
+        writer,
+        () => socket.dispose(),
+    );
+    const serverConnection = createServerProcess(
+        'Lua Language Server',
+        resolve(process.cwd(), './lua-language-server/bin/lua-language-server'),
+    )!;
+    forward(socketConnection, serverConnection);
+};
+
+const wsServer = new WebSocketServer({
+    port: 9999,
+});
+console.info(`WebSocketServer listening on port ${wsServer.options.port}`);
+wsServer.on("connection", (ws) => {
+    const socket: IWebSocket = {
+        send: (content) => {
+            ws.send(content, (error) => {
+                if (error) {
+                    throw error;
+                }
+            });
+        },
+        onMessage: (callback) => ws.on("message", callback),
+        onError: (callback) => ws.on("error", callback),
+        onClose: (callback) => ws.on("close", callback),
+        dispose: () => ws.close(),
+    }
+    if (ws.readyState === ws.OPEN) {
+        launch(socket);
+    } else {
+        ws.on("open", () => launch(socket));
+    }
+});
