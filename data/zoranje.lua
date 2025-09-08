7fd751eb740f2fb50e354df80a268c359e9e0237
Jonas Meeuws <jonas.meeuws@gmail.com>
2025-09-08 22:14:02 +0200

    build: Make ./deploy.sh less destructive.
    My apologies @thegamefire.

--- data/zoranje.lua
+++
@@ -1,2 +0,0 @@
--- je kan geen zoranje doen :(
-led.setAll(64, 32, 0)
--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,14 @@ if [ ! -e build-cross/ledstrip ]; then
     exit 1
 fi
 
+echo "Running ./pull-data.sh..."
+./pull-data.sh
+
+echo "Deleting server-side ledstrip folder..."
 ssh root@ledstrip rm -r ledstrip || true
+
 echo Running rsync...
 rsync -r data build-cross/ root@ledstrip:ledstrip/
-echo Restarting ledstrip
+
+echo Restarting ledstrip...
 ssh root@ledstrip systemctl restart ledstrip
